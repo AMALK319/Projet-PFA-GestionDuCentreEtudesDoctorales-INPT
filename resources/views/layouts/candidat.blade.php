6b46710605df1870074e4ffb259176bff2289415
amal kalim <amalkalim@gmail.com>
2021-07-01 01:33:30 +0100

    creer mininavbar

--- resources/views/layouts/candidat.blade.php
+++ resources/views/layouts/candidat.blade.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="fontawesome-free-5.15.1/css/all.css">
     <script src="https://kit.fontawesome.com/a076d05399.js"> </script>
 
     <script src="script.js" defer></script>
@@ -30,7 +36,7 @@
             background-color: #037CC2;
             color: #fff;
             width: 100%;
-             text-align: center;
+            text-align: center;
             font-family: Georgia, 'Times New Roman', Times, serif;
             font-size: large;
             height: 40px;
@@ -46,8 +52,6 @@
         body {
             height: 100%;
         }
-
-        
     </style>
 </head>
 
@@ -62,7 +66,11 @@
 
 
 
+
         <div class="content">
+           
+               
+          
             @yield("content")
 
         </div>
@@ -79,6 +87,9 @@
     </div>
 
     @yield("scripts")
+   
+    
+ 
     <script>
 
     </script>
---
+++ resources/views/partials/miniNavbar.php
@@ -0,0 +1,30 @@
+<div class="d-flex flex-row-reverse">
+    <div class="p-3">
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#037CC2" class="bi bi-person-circle " viewBox="0 0 16 16">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+        </svg>
+
+
+
+    </div>
+    <div class="p-3"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#037CC2" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+            <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z" />
+        </svg></div>
+    <div class="p-3">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#037CC2" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+        </svg>
+
+    </div>
+</div>
+
+
+
+
+
+
+<style>
+
+</style>
